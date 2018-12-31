df1d5dcf50612e083f93208f2c7411518a46c832
brnaldomesi <brnaldomesi1@gmail.com>
2018-12-31 15:48:50 +0300

    localizer added

---
+++ Sources/Snappy.Common.Client/Helpers/Localizer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Snappy.Common.Client.Helpers
+{
+    public static class Localizer
+    {
+        public static string Localize(this HtmlHelper helper, string key)
+        {
+            try
+            {
+                var text = key;// ((Dictionary<string, string>)Context.Application[Thread.CurrentThread.CurrentUICulture.Name])[key];
+                return string.IsNullOrEmpty(text) ? key : text;
+            }
+            catch { return key; }
+        }
+
+        public static string Localize(string key)
+        {
+            return Localize(null, key);
+        }
+    }
+}
