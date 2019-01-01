d3d138cc8163f32114ea0eaef94fb7014db503b8
brnaldomesi <brnaldomesi1@gmail.com>
2019-01-01 15:57:04 +0300

    logon, signup and change password view models added

---
+++ Sources/Snappy.Common.Client/Models/ViewModels/UserChangePasswordModel.cs
@@ -0,0 +1,66 @@
+﻿using Snappy.Common.Client.Helpers;
+using Snappy.Common.Client.Models.InputModels;
+using Snappy.Common.Helpers;
+
+namespace Snappy.Common.Client.Models.ViewModels
+{
+    public class UserChangePasswordModel : BaseModel
+    {
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ReEnterNewPassword { get; set; }
+
+        public PasswordInputModel OldPasswordInput { get; }
+        public PasswordInputModel NewPasswordInput { get; }
+        public PasswordInputModel ReEnterNewPasswordInput { get; }
+
+        public UserChangePasswordModel()
+        {
+            Title = Localizer.Localize("user_change_password_title");
+
+            OldPasswordInput = new PasswordInputModel("OldPassword", "old_password", true);
+            NewPasswordInput = new PasswordInputModel("NewPassword", "new_password", true);
+            ReEnterNewPasswordInput = new PasswordInputModel("ReEnterNewPassword", "re_enter_new_password", true);
+        }
+
+        public override void SetInputModelValues()
+        {
+            OldPasswordInput.Value = OldPassword;
+            NewPasswordInput.Value = NewPassword;
+            ReEnterNewPasswordInput.Value = ReEnterNewPassword;
+        }
+
+        public override void SetInputErrorMessages()
+        {
+            if (OldPassword.IsNotValidPassword())
+            {
+                OldPasswordInput.ErrorMessage.Add("password_is_not_valid_error_message");
+                InputErrorMessages.AddRange(OldPasswordInput.ErrorMessage);
+            }
+
+            if (NewPassword.IsNotValidPassword())
+            {
+                NewPasswordInput.ErrorMessage.Add("password_is_not_valid_error_message");
+                InputErrorMessages.AddRange(NewPasswordInput.ErrorMessage);
+            }
+
+            if (ReEnterNewPassword.IsNotValidPassword())
+            {
+                ReEnterNewPasswordInput.ErrorMessage.Add("password_is_not_valid_error_message");
+                InputErrorMessages.AddRange(ReEnterNewPasswordInput.ErrorMessage);
+            }
+
+            if (OldPassword == NewPassword)
+            {
+                NewPasswordInput.ErrorMessage.Add("new_password_can_not_same_as_old_password_error_message");
+                InputErrorMessages.AddRange(NewPasswordInput.ErrorMessage);
+            }
+
+            if (NewPassword != ReEnterNewPassword)
+            {
+                ReEnterNewPasswordInput.ErrorMessage.Add("re_entered_password_does_not_match_error_message");
+                InputErrorMessages.AddRange(ReEnterNewPasswordInput.ErrorMessage);
+            }
+        }
+    }
+}
