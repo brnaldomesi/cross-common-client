2017af362e3724d7a40e61db226b6bc9d7a744fa
brnaldomesi <brnaldomesi1@gmail.com>
2019-01-04 16:18:34 +0300

    IntegerInputModel implemented

---
+++ Sources/Snappy.Common.Client/Models/InputModels/IntegerInputModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Snappy.Common.Client.Models.InputModels
+{
+    public class IntegerInputModel : InputModel
+    {
+        public new int Value { get; set; }
+
+        public IntegerInputModel(string name, string labelKey, bool isRequired = false, int value = 0) : base(name, labelKey, isRequired, value.ToString())
+        {
+            Value = value;
+        }
+    }
+}
