7a45eaa8232968732740340544340260acc9961c
brnaldomesi <brnaldomesi1@gmail.com>
2019-01-07 10:37:20 +0300

    renamed integer input to number

---
+++ Sources/Snappy.Common.Client/Models/InputModels/NumberInputModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Snappy.Common.Client.Models.InputModels
+{
+    public class NumberInputModel : InputModel
+    {
+        public new int Value { get; set; }
+
+        public NumberInputModel(string name, string labelKey, bool isRequired = false, int value = 0) : base(name, labelKey, isRequired, value.ToString())
+        {
+            Value = value;
+        }
+    }
+}
