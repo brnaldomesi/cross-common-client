761dc2a69ec613e2bf0fa1abc586a112c3774ec3
brnaldomesi <brnaldomesi1@gmail.com>
2019-02-01 17:51:37 +0300

    unnecessary files deleted

--- Sources/Snappy.Common.Client/Models/Requests/Membership/IntegrationReadRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Snappy.Common.Client.Models.Requests.Membership
-{
-    public class IntegrationReadRequest : IntegrationBaseRequest
-    {
-        public IntegrationReadRequest(string integrationUid) : base(integrationUid)
-        {
-        }
-    }
-}
