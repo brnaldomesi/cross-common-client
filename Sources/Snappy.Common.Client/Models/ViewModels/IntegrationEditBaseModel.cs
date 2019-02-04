a48a6f47338e469d9501de1c2d727e2dc1fea10c
brnaldomesi <brnaldomesi1@gmail.com>
2019-02-04 12:08:09 +0300

    refactoring

--- Sources/Snappy.Common.Client/Models/ViewModels/IntegrationEditBaseModel.cs
+++ Sources/Snappy.Common.Client/Models/ViewModels/IntegrationEditBaseModel.cs
@@ -6,9 +6,11 @@ namespace Snappy.Common.Client.Models.ViewModels
 {
     public class IntegrationEditBaseModel : BaseModel
     {
+        public string IntegrationUid { get; set; }
         public string IntegrationName { get; set; }
         public string Description { get; set; }
 
+        public HiddenInputModel IntegrationUidInput { get; }
         public InputModel IntegrationNameInput { get; }
         public LongInputModel DescriptionInput { get; }
 
@@ -16,6 +18,7 @@ namespace Snappy.Common.Client.Models.ViewModels
         {
             Title = Localizer.Localize("integration_edit_title");
 
+            IntegrationUidInput = new HiddenInputModel("IntegrationUid","integration_uid");
             IntegrationNameInput = new InputModel("IntegrationName", "integration_name", true);
             DescriptionInput = new LongInputModel("Description", "description");
         }
