f6ba9426ace763703a6d2ca63f5defa3ce0dd6d4
brnaldomesi <brnaldomesi1@gmail.com>
2019-02-07 14:28:32 +0300

    some changes

--- Sources/Snappy.Common.Client/Models/ViewModels/BaseModel.cs
+++ Sources/Snappy.Common.Client/Models/ViewModels/BaseModel.cs
@@ -6,11 +6,15 @@ namespace Snappy.Common.Client.Models.ViewModels
     {
         public string Title { get; set; }
         public List<string> ErrorMessages { get; set; }
+        public List<string> SuccessMessages { get; set; }
+        public List<string> WarningMessages { get; set; }
         public List<string> InputErrorMessages { get; set; }
 
         protected BaseModel()
         {
             ErrorMessages = new List<string>();
+            SuccessMessages = new List<string>();
+            WarningMessages = new List<string>();
             InputErrorMessages = new List<string>();
         }
 
