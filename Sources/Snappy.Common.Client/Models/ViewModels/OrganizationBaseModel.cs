b67db45818312a2214c994c95b08cb26aa085759
brnaldomesi <brnaldomesi1@gmail.com>
2019-02-25 16:20:34 +0300

    language conroller review

---
+++ Sources/Snappy.Common.Client/Models/ViewModels/OrganizationBaseModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Snappy.Common.Client.Models.InputModels;
+using Snappy.Common.Helpers;
+
+namespace Snappy.Common.Client.Models.ViewModels
+{
+    public class OrganizationBaseModel : BaseModel
+    {
+        public Guid OrganizationUid { get; set; }
+
+        public HiddenInputModel OrganizationUidInput { get; set; }
+
+        public override void SetInputModelValues()
+        {
+            OrganizationUidInput.Value = OrganizationUid.ToString("N");
+        }
+
+        public override void SetInputErrorMessages()
+        {
+            if (OrganizationUid.IsEmptyGuid())
+            {
+                ErrorMessages.Add("organization_uid_is_not_valid");
+            }
+        }
+    }
+}
