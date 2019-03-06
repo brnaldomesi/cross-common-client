ffb3f6599b1676de13d564a2d840a4255ab74c8d
brnaldomesi <brnaldomesi1@gmail.com>
2019-03-06 09:40:48 +0300

    code review

---
+++ Sources/Snappy.Common.Client/Helpers/ModelHelper.cs
@@ -0,0 +1,16 @@
+﻿using Snappy.Common.Client.Models.ViewModels;
+using Snappy.Common.Models.Responses;
+
+namespace Snappy.Common.Client.Helpers
+{
+    public static class ModelHelper
+    {
+        public static void MapMessages(this BaseModel model, BaseResponse response)
+        {
+            model.ErrorMessages = response.ErrorMessages;
+            model.InfoMessages = response.InfoMessages;
+            model.WarningMessages = response.WarningMessages;
+            model.SuccessMessages = response.SuccessMessages;
+        }
+    }
+}
