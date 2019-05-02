0f65e7f727e2836bee3e14335f3214ae7d409f28
brnaldomesi <brnaldomesi1@gmail.com>
2019-05-02 20:36:40 +0300

    SelectPageParametersModel

---
+++ Sources/Snappy.Common.Client/Models/ViewModels/SelectPageParametersModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Snappy.Common.Client.Models.ViewModels
+{
+    public class SelectPageParametersModel
+    {
+        public Guid Parent { get; set; }
+
+        public Guid LastUid { get; set; }
+        public int Take { get; set; }
+
+        /// <summary>
+        /// search term
+        /// </summary>
+        public string Q { get; set; }
+    }
+}
