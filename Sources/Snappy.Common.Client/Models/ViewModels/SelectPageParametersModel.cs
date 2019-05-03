2ff6eb0448fc71a68a33215e19f63c25eb2aa925
brnaldomesi <brnaldomesi1@gmail.com>
2019-05-03 17:33:28 +0300

    id fix for select parameters

--- Sources/Snappy.Common.Client/Models/ViewModels/SelectPageParametersModel.cs
+++ Sources/Snappy.Common.Client/Models/ViewModels/SelectPageParametersModel.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using Snappy.Common.Helpers;
 
 namespace Snappy.Common.Client.Models.ViewModels
 {
     public class SelectPageParametersModel
     {
-        public Guid Parent { get; set; }
+        public Guid Id { get; set; }
+
+        private Guid _parent;
+        public Guid Parent
+        {
+            get
+            {
+                if (Id.IsNotEmptyGuid())
+                {
+                    _parent = Id;
+                }
+
+                return _parent;
+            }
+            set { _parent = value; }
+        }
 
         public Guid LastUid { get; set; }
         public int Take { get; set; }
