43782c8efe6c320c06040a148eff8c1335ea5878
brnaldomesi <brnaldomesi1@gmail.com>
2019-05-06 09:46:20 +0300

    table button  works

--- Sources/Snappy.Common.Client/Models/DataModels/DataResult.cs
+++ Sources/Snappy.Common.Client/Models/DataModels/DataResult.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Snappy.Common.Models.Shared;
 
 namespace Snappy.Common.Client.Models.DataModels
@@ -46,6 +47,21 @@ namespace Snappy.Common.Client.Models.DataModels
         public string PrepareLink(string url, bool isTargetBlank = true)
         {
             return PrepareLink(url, url, isTargetBlank);
+        }
+
+        public string PrepareButton(string name, string onClick, string className, string confirmTitle, string confirmContent)
+        {
+            return $"<button type='button' data-confirm-title='{confirmTitle}' data-confirm-content='{confirmContent}' class='btn {className}' onclick='{onClick}' data-translation='{name}'>{name}</button>";
+        }
+
+        public string PrepareDeleteButton(string name, string postUrl, string confirmTitle = "are_you_sure_you_want_to_delete_title", string confirmContent = "are_you_sure_you_want_to_delete_content")
+        {
+            return PrepareButton(name, $"handleDeleteRow(this, \"{postUrl}\")", "btn-delete", confirmTitle, confirmContent);
+        }
+
+        public string PrepareChangeActivationButton(string name, string postUrl, string confirmTitle = "are_you_sure_you_want_to_change_activation_title", string confirmContent = "are_you_sure_you_want_to_change_activation_content")
+        {
+            return PrepareButton(name, $"handleChangeActivationRow(this, \"{postUrl}\")", "btn-secondary", confirmTitle, confirmContent);
         }
     }
 
