98bb5c686d355c9ec3042bf0067a77dbfad86e5f
brnaldomesi <brnaldomesi1@gmail.com>
2019-05-14 17:44:54 +0300

    some tests passed

--- Tests/Snappy.Common.Client.Tests/Models/InputModels/SelectInputModelTests.cs
+++ Tests/Snappy.Common.Client.Tests/Models/InputModels/SelectInputModelTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 using Snappy.Common.Client.Models.InputModels;
 using static Snappy.Common.Tests.Helpers.BaseTestDataHelper;
@@ -7,7 +8,7 @@ namespace Snappy.Common.Client.Tests.Models.InputModels
     [TestFixture]
     public class SelectInputModelTests
     {
-        [TestCase(STRING, STRING_1, STRING_2, BOOL_TRUE, STRING_3, BOOL_TRUE, BOOL_TRUE, BOOL_TRUE, BOOL_TRUE, BOOL_TRUE, STRING_4)]
+        [TestCase(STRING, STRING_1, STRING_2, BOOL_TRUE, STRING_3, BOOL_TRUE, BOOL_TRUE, BOOL_TRUE, BOOL_FALSE, BOOL_TRUE, STRING_4)]
         [TestCase(STRING, STRING_1, STRING_2, BOOL_FALSE, STRING_3, BOOL_FALSE, BOOL_FALSE, BOOL_FALSE, BOOL_FALSE, BOOL_FALSE, STRING_4)]
         public void SelectInputModel(string name, string labelKey, string dataUrl, bool isRequired, string parent,
                                      bool isOptionTypeContent, bool isSetFirstItem, bool isAllOptionsVisible,
@@ -28,6 +29,20 @@ namespace Snappy.Common.Client.Tests.Models.InputModels
             Assert.AreEqual(isMultiple, model.IsMultiple);
             Assert.AreEqual(isHavingDetailInfo, model.IsHavingDetailInfo);
             Assert.AreEqual(detailInfoDataUrl, model.DetailInfoDataUrl);
+        }
+
+        [TestCase(STRING, STRING_1, STRING_2, BOOL_TRUE, STRING_3, BOOL_TRUE, BOOL_TRUE, BOOL_TRUE, BOOL_TRUE, BOOL_TRUE, STRING_4)]
+        public void SelectInputModel_ThrowArgumentException(string name, string labelKey, string dataUrl, bool isRequired, string parent,
+                                                            bool isOptionTypeContent, bool isSetFirstItem, bool isAllOptionsVisible,
+                                                            bool isMultiple, bool isHavingDetailInfo, string detailInfoDataUrl)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new SelectInputModel(name, labelKey, dataUrl, isRequired,
+                                     parent, isOptionTypeContent, isSetFirstItem,
+                                     isAllOptionsVisible, isMultiple, isHavingDetailInfo,
+                                     detailInfoDataUrl);
+            });
         }
     }
 }
