9f88f7b0d9d21ed0a19cf81f9644c19de6f0cb15
brnaldomesi <brnaldomesi1@gmail.com>
2019-06-01 09:37:12 +0300

    textarea work

--- Sources/Snappy.Common.Client/Models/InputModels/TextareaInputModel.cs
+++ Sources/Snappy.Common.Client/Models/InputModels/TextareaInputModel.cs
@@ -2,8 +2,13 @@
 {
     public class TextareaInputModel : InputModel
     {
+        public int Rows { get; set; }
+        public int Cols { get; set; }
+
         public TextareaInputModel(string name, string labelKey, bool isRequired = false, string value = "") : base(name, labelKey, isRequired, value)
         {
+            Rows = 5;
+            Cols = 70;
         }
     }
 }
