b89dbb4bac30440dbb81755b8c81489194b3b70c
brnaldomesi <brnaldomesi1@gmail.com>
2019-06-25 09:15:05 +0300

    base detail model added

---
+++ Sources/Snappy.Common.Client/Models/ViewModels/BaseDetailModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Snappy.Common.Models.DataTransferObjects;
+
+namespace Snappy.Common.Client.Models.ViewModels
+{
+    public abstract class BaseDetailModel : BaseModel
+    {
+        public string CreatedByName { get; set; }
+        public Guid CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public string UpdatedByName { get; set; }
+        public Guid? UpdatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public void MapCreatedAndUpdatedInfo(BaseDto dto)
+        {
+            CreatedBy = dto.CreatedBy;
+            CreatedByName = dto.CreatedByName;
+            CreatedAt = dto.CreatedAt;
+            UpdatedBy = dto.UpdatedBy;
+            UpdatedByName = dto.UpdatedByName;
+            UpdatedAt = dto.UpdatedAt;
+        }
+    }
+}
